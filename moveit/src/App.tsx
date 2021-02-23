8adcb79e535e92131aa82bdd00f3ef047581288a
John Kleber <jo.kleber85@gmail.com>
2021-02-23 15:40:33 -0300

    Aula 01 concluída V

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ExperienceBar from './components/ExperienceBar';
+//import { Button } from './components/Button';
+import './styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+{/*   <h1>Hello Word</h1>
+      <Button color = "blue">
+        Botao 1
+      </Button>
+      <Button color = "red">
+        Botao 2
+      </Button>
+      <Button color = "green">
+        Botao 3  
+      </Button> */}
+      <div className="container">
+        <ExperienceBar />
+      </div>
+    </>
   );
 }
 
